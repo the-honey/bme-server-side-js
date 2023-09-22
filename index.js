1b2e9dc38c13e9b0cd2522a8053c7e576c8313e6
Mézes István <1valaki1asd@gmail.com>
2023-09-22 23:28:47 +0200

    Change routing

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = 3000;
 
-app.use('/', express.static('static'));
+app.get('/', (req, res, next) => {
+  res.redirect('/artist/');
+});
+app.use('/', express.static('pages'));
+app.use('/assets/', express.static('assets'));
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
