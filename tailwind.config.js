2f9810a9c4c85121fff2181f6efef012b943727d
Mézes István <1valaki1asd@gmail.com>
2023-10-12 20:05:53 +0200

    Remove component library cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./**/*.{html,js}'],
+export default {
+  content: ['./views/**/*.{html,js}'],
   theme: {
     extend: {},
   },
   plugins: [daisyui],
-  corePlugins: {
-    // due to https://github.com/tailwindlabs/tailwindcss/issues/6602 - buttons disappear
-    preflight: false,
-  },
 };
